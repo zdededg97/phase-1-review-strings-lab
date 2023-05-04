680491fa9e74fa1affb699015a6ae7f7ecafaeb5
Edgar Hernandez <zdededg@gmail.com>
2023-05-04 13:17:43 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+console.log(shortGreeting)
+
+
+  
+ 
